6aae4e70052aefc868735cb2e4dc6ff4f1df9e82
Andrés Castellanos <andres.cv@galileo.edu>
2019-01-30 16:05:23 -0600

    update lab1 autograder

--- autograders/autograder/files/lab1_c_gdb/check.py
+++ autograders/autograder/files/lab1_c_gdb/check.py
@@ -1,38 +1,43 @@
-from .. import utils
+import utils
+import resource
+import subprocess
 
 
-__ALL__ = ['lab1_c_gdb_expected', 'lab1_c_gdb']
+# 10 MiB
+BYTES = 10 * 1024 * 1024
 
 
-lab1_c_gdb_expected = ['eccentric.c', 'ex2.txt', 'll_equal.c', 'll_cycle.c']
-
-
-def check_eccentric(tmp):
+def check_eccentric():
     try:
-        task = utils.make(target='test_eccentric', dir=tmp)
+        task = utils.make(target='test_eccentric')
         if task.returncode != 0:
-            return (0, utils.failed('Compilation error'), task.stderr.decode().strip())
-        task = utils.execute('./test_eccentric', dir=tmp, timeout=1)
+            return (0, utils.failed('compilation error'), task.stderr.decode().strip())
+        task = utils.execute('./test_eccentric', timeout=1)
         if task.returncode != 0:
-            return (0, utils.failed('Runtime error'), task.stderr.decode().strip())
+            return (0, utils.failed('runtime error'), task.stderr.decode().strip())
         # Output
         output = task.stdout.decode().strip()
         expected = 'V0 OK\nV1 OK\nV2 OK\nV3 OK'
         expected = expected.split('\n')
         output = output.split('\n')
         grade = 0
+        wrong = 0
         for (exp, out) in zip(expected, output):
             exp = exp.strip()
             out = out.strip()
             if exp == out:
                 grade += 25/4
-        return (grade, utils.passed() if grade == 25 else utils.incomplete('Some answers are wrong...'), '')
+            else:
+                wrong += 1
+        return (round(grade), utils.passed() if wrong == 0 else utils.incomplete('some answers are wrong...'), '')
+    except subprocess.TimeoutExpired:
+        return (0, utils.failed('TIMEOUT'), '')
     except Exception:
-        return (0, utils.failed('TIMEOUT.'), '')
+        return (0, utils.failed('memory limit exceeded'))
 
 
-def check_cgdb(tmp):
-    output = utils.parse_form(utils.join(tmp, 'ex2.txt'))
+def check_cgdb():
+    output = utils.parse_form('ex2.txt')
     expected = {'1': 'b', '2': 'c', '3': 'd', '4': 'b', '5': 'c', '6': 'c', '7': 'a', '8': 'a', '9': 'b'}
     grade = 0
     wrong = []
@@ -43,17 +48,17 @@ def check_cgdb(tmp):
             grade += 25/9
         else:
             wrong.append('q'+key)
-    return (grade, utils.passed() if len(wrong) == 0 else utils.incomplete(','.join(wrong)))
+    return (round(grade), utils.passed() if len(wrong) == 0 else utils.incomplete(','.join(wrong)))
 
 
-def check_equal(tmp):
+def check_equal():
     try:
-        task = utils.make(target='test_ll_equal', dir=tmp)
+        task = utils.make(target='test_ll_equal')
         if task.returncode != 0:
-            return (0, utils.failed('Compilation error'), task.stderr.decode().strip())
-        task = utils.execute('./test_ll_equal', dir=tmp, timeout=1)
+            return (0, utils.failed('compilation error'), task.stderr.decode().strip())
+        task = utils.execute('./test_ll_equal', timeout=1)
         if task.returncode != 0:
-            return (0, utils.failed('Runtime error'), task.stderr.decode().strip())
+            return (0, utils.failed('cuntime error'), task.stderr.decode().strip())
         output = task.stdout.decode().strip()
         expected = 'OK\nOK\n'
         expected = expected.strip()
@@ -61,16 +66,18 @@ def check_equal(tmp):
         if expected == output:
             grade += 25
         return (grade, utils.passed() if grade == 25 else utils.failed('Failed some tests...'), '')
-    except Exception:
+    except subprocess.TimeoutExpired:
         return (0, utils.failed('TIMEOUT'), '')
+    except Exception:
+        return (0, utils.failed('memory limit exceeded'))
 
 
-def check_ll_cycle(tmp):
+def check_ll_cycle():
     try:
-        task = utils.make(target='test_ll_cycle', dir=tmp)
+        task = utils.make(target='test_ll_cycle')
         if task.returncode != 0:
             return (0, utils.failed('Compilation error'), task.stderr.decode().strip())
-        task = utils.execute('./test_ll_cycle', dir=tmp, timeout=1)
+        task = utils.execute('./test_ll_cycle', timeout=1)
         if task.returncode != 0:
             return (0, utils.failed('Runtime error'), task.stderr.decode().strip())
         output = task.stdout.decode().strip()
@@ -80,27 +87,42 @@ def check_ll_cycle(tmp):
         if expected == output:
             grade += 25
         return (grade, utils.passed() if grade == 25 else utils.failed('Failed some tests...'), '')
-    except Exception:
+    except subprocess.TimeoutExpired:
         return (0, utils.failed('TIMEOUT'), '')
+    except Exception:
+        return (0, utils.failed('memory limit exceeded'))
 
 
-def lab1_c_gdb(tmp, token):
-    table = []
-    eccentric = check_eccentric(tmp)
-    table.append(['EX1: eccentric', eccentric[0], eccentric[1]])
-    cgdb = check_cgdb(tmp)
-    table.append(['EX2: CGDB', cgdb[0], cgdb[1]])
-    ll_equal = check_equal(tmp)
-    table.append(['Ex3: ll_equal', ll_equal[0], ll_equal[1]])
-    ll_cycle = check_ll_cycle(tmp)
-    table.append(['Ex5: ll_cycle', ll_cycle[0], ll_cycle[1]])
-    grade = 0
-    grade += eccentric[0]
-    grade += cgdb[0]
-    grade += ll_equal[0]
-    grade += ll_cycle[0]
-    errors = ''
-    errors += utils.create_error('eccentric.c', eccentric[2])
-    errors += utils.create_error('ll_equal.c', ll_equal[2])
-    errors += utils.create_error('ll_cycle.c', ll_cycle[2])
-    return (grade, table, errors)
+def lab1_c_gdb():
+    not_found = utils.expected_files(['./eccentric.c', './ex2.txt', './ll_equal.c', './ll_cycle.c'])
+    if len(not_found) == 0:
+        table = []
+        eccentric = check_eccentric()
+        table.append(['1. eccentric', eccentric[0], eccentric[1]])
+        cgdb = check_cgdb()
+        table.append(['2. CGDB', cgdb[0], cgdb[1]])
+        ll_equal = check_equal()
+        table.append(['3. ll_equal', ll_equal[0], ll_equal[1]])
+        ll_cycle = check_ll_cycle()
+        table.append(['5. ll_cycle', ll_cycle[0], ll_cycle[1]])
+        grade = 0
+        grade += eccentric[0]
+        grade += cgdb[0]
+        grade += ll_equal[0]
+        grade += ll_cycle[0]
+        errors = ''
+        errors += utils.create_error('eccentric.c', eccentric[2])
+        errors += utils.create_error('ll_equal.c', ll_equal[2])
+        errors += utils.create_error('ll_cycle.c', ll_cycle[2])
+        grade = min(grade, 100)
+        report = utils.report(table)
+        if errors != '':
+            report += '\n\nMore Info:\n\n' + errors
+        return utils.write_result(grade, report)
+    else:
+        utils.write_result(0, 'missing files: %s' % (','.join(not_found)))
+
+
+if __name__ == '__main__':
+    resource.setrlimit(resource.RLIMIT_AS, (BYTES, BYTES))
+    lab1_c_gdb()
